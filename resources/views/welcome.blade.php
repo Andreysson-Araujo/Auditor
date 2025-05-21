64fee626e5208f8a90de307981d447d1b2915c71
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2025-05-21 10:28:22 -0500

    iniciando estrutura

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bem-vindo ao Sistema</title>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Bem-vindo ao  Auditor</h1>
+        </header>
+
+        <main>
+            <section>
+                <h2>Como Funciona?</h2>
+                <p>
+                    Esse sistem faz o uso da coleta de dados utilizados no the questiom para Realizarmos Feedbacks das manifestações.
+                </p>
+                <H3>Porque isso?</H3>
+                <ul>
+                    <li><strong>Implementar Melhorias:</strong> Melhorar nossas ações para Implementar na saude mental do servidor.</li>
+                    <li><strong>Saber como esta o ambiente:</strong>Para um trabalho saudavel prescisamos de um ambiente saudavel</li>
+                </ul>
+
+                <p>Deseja prosseguir respondendo nosso Questionario?</p>
+                <div>
+                   
+                </div>
+            </section>
+        </main>
+
+        <footer>
+            <p>&copy; {{ date('Y') }} Sistema de Gestão</p>
+        </footer>
+    </div>
+</body>
+</html>
